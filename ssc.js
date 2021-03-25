45ae9f063df693eaeb521b05d0e3debdf3fd1e70
justjcurtis <justjcurtis@gmail.com>
2021-03-25 23:47:19 +0000

    vastly improved checker (only does ends not loops)

--- ssc.js
+++ ssc.js
@@ -1,5 +1,20 @@
 const Graph = require('./lib/models/graph')
 
-const g = new Graph(1, 25);
-
-console.log(g.squareSumsMap)
+let max = 25
+while(max <= 10000){
+    const g = new Graph(1, max);
+    
+    let endCount = 0
+    for(let i = g.min; i <= g.max; i++){
+        const arr = g[i]
+        if(g.squareSumsMap[i].length == 1){
+            endCount ++
+        }
+        if(endCount > 2){
+            console.log(`failure for max of ${g.max}`)
+            throw("end")
+        }
+    }
+    console.log(`solutions possible for max of ${g.max}`)
+    max++
+}
