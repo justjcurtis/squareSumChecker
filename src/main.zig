411d8120da20c021b1c5d2fa8a8a0fcad7db291e
justjcurtis <justjcurtis@gmail.com>
2025-03-13 00:40:53 +0000

    feat: implemented graph model

--- src/main.zig
+++ src/main.zig
@@ -2,17 +2,30 @@ const std = @import("std");
 const Graph = @import("models/graph.zig").Graph;
 
 pub fn main() !void {
-    std.debug.print("Hello, world!\n", .{});
     var graph = Graph{ .min = 1, .max = 10 };
     try graph.init();
     defer graph.deinit();
 
     std.debug.print("Min: {d}\nMax: {d}\n", .{ graph.min, graph.max });
 
-    // Now this should work without segmentation fault
     std.debug.print("Squares from main: ", .{});
-    for (graph.squares) |square| {
-        std.debug.print("{d} ", .{square});
+    var squaresItr = graph.squares.valueIterator();
+    while (squaresItr.next()) |valuePtr| {
+        const value = valuePtr.*;
+        std.debug.print("{d} ", .{value});
+    }
+    std.debug.print("\n", .{});
+
+    std.debug.print("Square sums map from main: \n", .{});
+    var squareSumsMapItr = graph.square_sums_map.iterator();
+    while (squareSumsMapItr.next()) |kv| {
+        const key = kv.key_ptr.*;
+        const value = kv.value_ptr.*;
+        std.debug.print("\t{d}: ", .{key});
+        for (value.items) |item| {
+            std.debug.print("{d} ", .{item});
+        }
+        std.debug.print("\n", .{});
     }
     std.debug.print("\n", .{});
 }
--- src/models/graph.zig
+++ src/models/graph.zig
@@ -3,33 +3,53 @@ const std = @import("std");
 pub const Graph = struct {
     min: u32 = 1,
     max: u32 = 1000,
-    squares: []u32 = undefined,
-    square_sums_map: [][]u32 = undefined,
+    squares: std.AutoHashMap(u32, u32) = undefined,
+    square_sums_map: std.AutoHashMap(u32, std.ArrayList(u32)) = undefined,
     allocator: std.mem.Allocator = undefined,
 
     pub fn init(self: *Graph) !void {
+        // get stdout
+        const stdout = std.io.getStdOut().writer();
+        try stdout.print("Initializing graph...\n", .{});
         if (self.max < self.min) {
             self.max = self.min + 1;
         }
-        const num_count = self.max + 1 - self.min;
 
         self.allocator = std.heap.page_allocator;
+        self.squares = std.AutoHashMap(u32, u32).init(self.allocator);
 
-        self.squares = try self.allocator.alloc(u32, num_count);
-
-        var i: u32 = 0;
-        while (i < num_count) : (i += 1) {
-            self.squares[i] = (i + self.min) * (i + self.min);
+        var sqrt: u32 = 1;
+        while (true) {
+            const square = sqrt * sqrt;
+            if (square > self.max) break;
+            try self.squares.put(square, square);
+            sqrt += 1;
         }
 
-        // Debug print inside init
-        for (self.squares) |square| {
-            std.debug.print("{d} ", .{square});
+        self.square_sums_map = std.AutoHashMap(u32, std.ArrayList(u32)).init(self.allocator);
+        for (self.min..self.max) |i| {
+            const a: u32 = @as(u32, @intCast(i));
+            for (i + 1..self.max + 1) |j| {
+                const b: u32 = @as(u32, @intCast(j));
+                const sum = a + b;
+                if (self.squares.contains(sum)) {
+                    if (self.square_sums_map.contains(sum)) {
+                        var list = self.square_sums_map.get(sum).?;
+                        try list.append(a);
+                        try list.append(b);
+                    } else {
+                        var list = std.ArrayList(u32).init(self.allocator);
+                        try list.append(a);
+                        try list.append(b);
+                        try self.square_sums_map.put(sum, list);
+                    }
+                }
+            }
         }
-        std.debug.print("\n", .{});
     }
 
     pub fn deinit(self: *Graph) void {
-        self.allocator.free(self.squares);
+        self.squares.deinit();
+        self.square_sums_map.deinit();
     }
 };
